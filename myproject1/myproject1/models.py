878ea1fe235b59aa755eb4c6fc670ec2cff6358a
nowshadlona <nowshad_j@yahoo.com>
2024-01-03 18:52:27 -0500

    admin panel

---
+++ myproject1/myproject1/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class About(models.Model):
+    u_name = models.CharField(max_length=500)
+    dob = models.CharField(max_length=500)
+    phone = models.CharField(max_length=500)
+    email = models.CharField(max_length=500)
+    no_exp = models.CharField(max_length=500)
+    no_happy_customers = models.CharField(max_length=500)
+    no_project_finished = models.CharField(max_length=500)
+    no_digital_awards = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    date_time = models.CharField(max_length=5000)
+
+class companies(models.Model):
+    image = models.ImageField(null=True, blank=True, upload_to ="images/")
